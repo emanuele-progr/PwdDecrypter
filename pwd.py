860221117a5e62e00dc654a6ffc0d2ccdc8226e8
emanuele-progr <marioemanuele.ghianni@stud.unifi.it>
2019-12-29 18:56:30 +0100

    updated pwd script

--- pwd.py
+++ pwd.py
@@ -1,19 +1,24 @@
-import string
+def remove_duplicates(infile):
+    storehouse = set()
+    with open("outfile.txt","w+", encoding="Latin-1") as out:
+        for line in open(infile):
+            if line not in storehouse:
+                out.write(line)
+                storehouse.add(line)
+                
+lines = open('rockyou.txt', 'r', encoding="Latin-1").readlines()
 
-myList = []
-filepath = 'rockyou.txt'
-with open(filepath, encoding="Latin-1") as fp:
-   line = fp.readline()
-   cnt = 1
-   while line:
-       myList.append(line)
-       line = fp.readline()
-       cnt += 1
 
-result = [s for s in myList if not (s.strip("qwertyuiopasdfghjklzxcvbQWERTYUIOPASDFGHJKLZXCVB" +"\n"  + "1234567890") or len(s) != 9)]
+
+result = [s for s in lines if not (s.strip("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM\." +"\n"  + "1234567890") or len(s) != 9)]
 file2 = open("psw.txt","a", encoding="Latin-1")
-for s in result:
+lines_set = set(result)
+count = 0
+for s in lines_set:
     file2.write(s)
+    count += 1
 
+print(count)
+remove_duplicates("psw.txt")
 
 
